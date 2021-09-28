8050c39198a7ef87e4f6746531f524fe16f181f9
dhef415 <dhef415@gmail.com>
2021-09-28 13:01:30 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
-// implement your posts router here
+const router = require('express').Router()
+const Posts = require('./posts-model')
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.status(200).json(posts)
+    } catch (err) {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// require your server and launch it here
+const server = require('./api/server')
+
+server.listen(5000, () => {
+    console.log('**** Listening on 5000 ****')
+})
